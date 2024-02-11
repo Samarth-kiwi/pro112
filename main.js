8f517bec1416f9f41e771503e059e78e13a5ec57
Samarth-kiwi <128691674+Samarth-kiwi@users.noreply.github.com>
2024-02-11 14:03:58 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function snapshot(){
+    <script src=" https://unpkg.com/ml5@0.4.3/dist/ml5.min.js "></script>
+    https://teachablemachine.withgoogle.com/models/[...]model.json;
+};
+function check(){
+
+};
+gotResult(){
+if{
+&#9996;
+&#128076;
+&#128077;
+};
+};
+function speak(){};
